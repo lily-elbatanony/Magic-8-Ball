10710efedbdeebb0bd4f40916ca517b922170aa0
lily-elbatanony <lily.rmagdy@gmail.com>
2025-06-28 13:43:08 +0300

    display functionality

---
+++ main.py
@@ -0,0 +1,24 @@
+import random
+responses=["Yes, definitely!",
+           "No, not now,",
+           "Ask again later.",
+           "Its certain.",
+           "Very doubtful",
+           "Outlook is good.",
+           "Better not tell you now.",
+           "Concentrate and ask again."]
+def get_random_response():
+    return random.choice(responses)
+def get_user_guess():
+    while True:
+        try:
+            guess=int(input("Enter your guess(1-100)"))
+            if 1<=guess<=100:
+                return guess
+            else:
+                print("Please enter a number between 1 and 100.")
+        except ValueError:
+            print("Invalid input! Please enter a number.")
+            
+def display_response(response):
+    print("\nThe Magic 8-Ball says: ", response, "\n")
