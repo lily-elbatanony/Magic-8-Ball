cd0286a1f6473f3dcf9b7fa2469404c9176d0c43
lily-elbatanony <lily.rmagdy@gmail.com>
2025-06-28 13:21:48 +0300

    random response feature

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import random
+responses=["Yes, definitely!",
+           "No, not now,",
+           "Ask again later.",
+           "Its certain.",
+           "Very doubtful",
+           "Outlook is good.",
+           "Better not tell you now.",
+           "Concentrate and ask again."]
+def get_random_response():
+    return random.choice(responses)
 def get_user_guess():
     while True:
         try:
